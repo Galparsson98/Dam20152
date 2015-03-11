5619c3fb448e0b09fb0c668ad05dcebcae91ffd4
Galparsson98 <mikgalpar@gmail.com>
2015-03-11 13:45:02 +0100

    Ajaxdos

--- Ejercicios ajax/Ejercicio6/servidor/Cargamunicipios.js
+++ Ejercicios ajax/Ejercicio6/servidor/Cargamunicipios.js
@@ -3,11 +3,83 @@
       var kom2= document.getElementById("Combo2");
       
   }
-  function llamadaxml(){
-      if(window.XMLHttpRequest) {
+  var net = new Object();
+ 
+net.READY_STATE_UNINITIALIZED=0; 
+net.READY_STATE_LOADING=1; 
+net.READY_STATE_LOADED=2; 
+net.READY_STATE_INTERACTIVE=3; 
+net.READY_STATE_COMPLETE=4; 
+ 
+// Constructor
+net.CargadorContenidos = function(url, funcion, funcionError, metodo, parametros, contentType) {
+  this.url = url;
+  this.req = null;
+  this.onload = funcion;
+  this.onerror = (funcionError) ? funcionError : this.defaultError;
+  this.cargaContenidoXML(url, metodo, parametros, contentType);
+}
+ 
+net.CargadorContenidos.prototype = {
+  cargaContenidoXML: function(url, metodo, parametros, contentType) {
+    if(window.XMLHttpRequest) {
       this.req = new XMLHttpRequest();
     }
     else if(window.ActiveXObject) {
       this.req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+ 
+    if(this.req) {
+      try {
+        var loader = this;
+        this.req.onreadystatechange = function() {
+          loader.onReadyState.call(loader);
+        }
+        this.req.open(metodo, url, true);
+        if(contentType) {
+          this.req.setRequestHeader("Content-Type", contentType);
+        }
+        this.req.send(parametros);
+        } catch(err) {
+          this.onerror.call(this);
+        }
+    }
+  },
+ 
+  onReadyState: function() {
+    var req = this.req; 
+    var ready = req.readyState; 
+    if(ready == net.READY_STATE_COMPLETE) { 
+      var httpStatus = req.status; 
+      if(httpStatus == 200 || httpStatus == 0) { 
+        this.onload.call(this);
+      }
+      else {
+        this.onerror.call(this);
+      }
+    }
+  },
+ 
+  defaultError: function() {
+    alert("Se ha producido un error al obtener los datos"
+      + "\n\nreadyState:" + this.req.readyState
+      + "\nstatus: " + this.req.status
+      + "\nheaders: " + this.req.getAllResponseHeaders());
   }
-  } 
+}
+function muestraContenido() {
+  if(peticion_http.readyState == 4) {
+    if(peticion_http.status == 200) {
+      alert(peticion_http.responseText);
+    }
+  }
+    function cargaContenido(url, metodo, funcion) {
+  peticion_http = inicializa_xhr();
+ 
+  if(peticion_http) {
+    peticion_http.onreadystatechange = funcion;
+    peticion_http.open(metodo, url, true);
+    peticion_http.send(null);
+  }
+}
+}
