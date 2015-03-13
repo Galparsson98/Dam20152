e11a497de71a52c0dcfc5bd190b7d845e898677e
Galparsson98 <mikgalpar@gmail.com>
2015-03-13 14:20:49 +0100

    fren

--- Ejercicio/js/app/main.js
+++ Ejercicio/js/app/main.js
@@ -1,57 +1,61 @@
-//objeto global.
-var APP = APP || {};
-$.document.ready(function objglobal(){
-    var caracteristicas =function objglobal (){
-    var cosa1 =$('caracteristicas eliminados');
-    var cosa2 =$('caracteristicas contenedor');
-    var cosa3 =$('caracteristicas botones');
-    }
-    urlBase= '/php/getcards.php';
+$.document.ready(function objglobal() {
+            var cosa1 = $('caracteristicas eliminados');
+            var cosa2 = $('caracteristicas contenedor');
+            var cosa3 = $('caracteristicas botones');
+
+            urlBase = '/php/getcards.php';
 
 
-}
-var ima1= $('<img/>',{ src='img/cat.png');
-var ima2= $('<img/>',{ src='img/dog.png');
-         var filak =$('#filas');
-        var kolumna = $('#columnas');
-var imagenes = function imagen(ima1, ima2){
-                      var i=0;
+            var ima1 = $('<img/>', {
+                src: 'img/cat.png'
+            });
+            var ima2 = $('<img/>', {
+                src: 'img/dog.png'
+            });
+            var filak = $('#filas');
+            var kolumna = $('#columnas');
+            var imagenes = function imagen(ima1, ima2) {
+                var i = 0;
 
-            for (i=0, i<filak; i++)
-                      {
-                        }
-                 for (i=0, i<kolumna; i++)
-                      {
-                           }
-function avisaanet (datos) {
-       console.log(datos);
+                for (i = 0; i < filak; i++) {
 
-}
-}
+                }
+                for (i = 0; i < kolumna; i++) {
 
-//Usar "use strict".
+                }
 
-//esperar a que el DOM este cargado.
+            }
 
+            function avisaanet(datos) {
+                console.log(datos);
 
-// Iniciaremos las variables privadas que sean necesarias.
+            }
 
-// Necesitaremos una funcion que cree objetos DOM de clase img y todos los atributos necesarios.
-// Necesitaremos una funcion que agrege el array de img al DOM (no se realizaran cambios de DOM dentro de ningun bucle).
+            function pasodetiempo() {
 
-// Necesitaremos una o varias funcion(es) que controle(n) el paso del tiempo.
+            }
 
-// Necesitaremos añadir los eventos necesarios en el momento adecuado.
+        }
+        //Usar "use strict".
 
-// Necesitaremos las funciones de callback para los eventos.
-
-// Necesitaremos una funcion encargada de llamar al modulo que se define en el fichero net.js con los parametros adecuados para realizar la llamada y capturar la respuesta AJAX.
-
-// Necesitaremos una funcion que controle el final del juego.
-
-// Necesitaremos una funcion que controle la lista de eliminados.
-
-// Cualquier otra funcion que sea necesaria.
+        //esperar a que el DOM este cargado.
 
 
+        // Iniciaremos las variables privadas que sean necesarias.
 
+        // Necesitaremos una funcion que cree objetos DOM de clase img y todos los atributos necesarios.
+        // Necesitaremos una funcion que agrege el array de img al DOM (no se realizaran cambios de DOM dentro de ningun bucle).
+
+        // Necesitaremos una o varias funcion(es) que controle(n) el paso del tiempo.
+
+        // Necesitaremos añadir los eventos necesarios en el momento adecuado.
+
+        // Necesitaremos las funciones de callback para los eventos.
+
+        // Necesitaremos una funcion encargada de llamar al modulo que se define en el fichero net.js con los parametros adecuados para realizar la llamada y capturar la respuesta AJAX.
+
+        // Necesitaremos una funcion que controle el final del juego.
+
+        // Necesitaremos una funcion que controle la lista de eliminados.
+
+        // Cualquier otra funcion que sea necesaria.
